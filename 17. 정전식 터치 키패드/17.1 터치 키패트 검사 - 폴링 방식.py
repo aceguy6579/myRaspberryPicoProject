31bde939431daa5219abaa062db46083809f0734
josh <aceguy6579@gmail.com>
2025-11-06 19:21:03 +0900

    20251106

--- 17. 정전식 터치 키패드/17.1 터치 키패트 검사 - 폴링 방식.py
+++ 17. 정전식 터치 키패드/17.1 터치 키패트 검사 - 폴링 방식.py
@@ -3,13 +3,13 @@ import time
 import mpr121
 
 i2c = I2C(0)
-mpr = mpr121.MPR121(i2c)
+mpr = mpr121.MPR121(i2c, address=0x5A) # 터치 키패드 제어 객체
 
 while True:
-    state = mpr.touched()
+    state = mpr.touched() # 12개 센서 상태 읽기
     print('{0:5d}: '.format(state), end='')
 
-for e in range(12):
+for e in range(12): # 비트벼로 센서 상태 출력
     if state & (1 << e):
         print('0', end='')
     else:
